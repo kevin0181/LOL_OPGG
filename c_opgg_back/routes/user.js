6711f5675c4a403ff0b89aeaa08c31b726c3c854
youngbeen you <59413865+kevin0181@users.noreply.github.com>
2025-04-18 17:09:32 +0900

    -

--- c_opgg_back/routes/user.js
+++ c_opgg_back/routes/user.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const jwt = require('jsonwebtoken');
+let authenticateToken = require('./../jwtConfig');
 
-const RIOT_API_KEY = 'RGAPI-dfc9773a-35f1-47e9-bac9-c6144b97d7c0'; // 발급받은 API 키
+require('dotenv').config();
+
+const RIOT_API_KEY = 'RGAPI-5102e48a-e588-44bd-a377-47b40b0092dc'; // 발급받은 API 키
 const gameName = 'Hide on bush'; // 닉네임
 const tagLine = 'KR1'; // 태그라인
 
@@ -25,14 +29,46 @@ async function getSummonerInfo(gameName, tagLine) {
     });
 
     console.log(summonerResponse.data);
+
+    return summonerResponse.data;
+  } catch (error) {
+    console.error('Error fetching data:', error.response ? error.response.data : error.message);
+  }
+};
+
+let generateToken = (puuid) => {
+  return jwt.sign({ puuid }, process.env.JWT_SECRET_KEY);
+};
+
+const getUserRank = async (puuid) => {
+  try {
+
+    console.log(puuid);
+
+    const getRank = await axios.get(`https://kr.api.riotgames.com/lol/league/v4/entries/by-puuid/${puuid}`, {
+      headers: {
+        'X-Riot-Token': RIOT_API_KEY
+      }
+    });
+
+    return getRank.data[0];
+
   } catch (error) {
     console.error('Error fetching data:', error.response ? error.response.data : error.message);
   }
 }
 
+router.get('/', async (req, res) => {
 
-router.get('/', (req, res) => {
-  getSummonerInfo(gameName, tagLine);
+  let userInfo = await getSummonerInfo(gameName, tagLine);
+  userInfo.jwt_puuid = generateToken(userInfo.puuid);
+
+  res.json(userInfo);
+
+});
+
+router.get('/rank', authenticateToken, async (req, res) => {
+  res.json(await getUserRank(req.user.puuid));
 });
 
 
