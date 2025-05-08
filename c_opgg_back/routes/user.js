1045c392786b83e4d3d7266e314b04943e830407
youngbeen you <59413865+kevin0181@users.noreply.github.com>
2025-05-08 14:00:29 +0900

    -

--- c_opgg_back/routes/user.js
+++ c_opgg_back/routes/user.js
@@ -6,7 +6,7 @@ let authenticateToken = require('./../jwtConfig');
 
 require('dotenv').config();
 
-const RIOT_API_KEY = 'RGAPI-5102e48a-e588-44bd-a377-47b40b0092dc'; // 발급받은 API 키
+const RIOT_API_KEY = 'RGAPI-296b2b70-f420-413a-88be-935c76d6a4a5'; // 발급받은 API 키
 const gameName = 'Hide on bush'; // 닉네임
 const tagLine = 'KR1'; // 태그라인
 
@@ -58,6 +58,65 @@ const getUserRank = async (puuid) => {
   }
 }
 
+let getMatchIds = async (puuid, start, count) => {
+  try {
+    const ids = await axios.get(`https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=${start}&count=${count}`, {
+      headers: {
+        'X-Riot-Token': RIOT_API_KEY
+      }
+    });
+
+    return ids.data;
+  } catch (error) {
+    console.error('Error fetching data:', error.response ? error.response.data : error.message);
+  }
+}
+
+let getMatchSummary = async (ids) => {
+
+  try {
+    const matchSummaries = await Promise.all(ids.map(async (matchId) => {
+      const res = await axios.get(`https://asia.api.riotgames.com/lol/match/v5/matches/${matchId}`, {
+        headers: {
+          'X-Riot-Token': RIOT_API_KEY
+        }
+      });
+
+      const data = res.data;
+
+      return {
+        matchId: matchId,
+        champion: data.championName,
+        level: data.champLevel,
+        kda: `${data.kills}/${data.deaths}/${data.assists}`,
+        win: data.win,
+        items: [
+          data.item0,
+          data.item1,
+          data.item2,
+          data.item3,
+          data.item4,
+          data.item5,
+          data.item6
+        ],
+        name: data.riotIdGameName || data.summonerName,
+        tag: data.riotIdTagline || '',
+        position: data.individualPosition,
+        cs: data.totalMinionsKilled,
+        damage: data.totalDamageDealtToChampions,
+        vision: data.visionScore,
+        duration: data.info.gameDuration
+      };
+    }));
+
+    return matchSummaries;
+  } catch (error) {
+    console.error('Error fetching match summaries:', error.response ? error.response.data : error.message);
+    return [];
+  }
+}
+
+
 router.get('/', async (req, res) => {
 
   let userInfo = await getSummonerInfo(gameName, tagLine);
@@ -71,6 +130,12 @@ router.get('/rank', authenticateToken, async (req, res) => {
   res.json(await getUserRank(req.user.puuid));
 });
 
+router.get('/matches', async (req, res) => {
+  let userInfo = await getSummonerInfo(gameName, tagLine);
+  let ids = await getMatchIds(userInfo.puuid, 0, 20);
+
+  res.json(await getMatchSummary(ids));
+});
 
 
 module.exports = router;
